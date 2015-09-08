428b1b6b61233c16606fb0665ff521135c96684a
suǝʞǝǝpʇ <tobias.deekens@epost-dev.de>
2015-09-08 23:48:31 +0200

    Fix browser name (one does not simply lowercase :bomb:.

--- grunt/tasks/karma.js
+++ grunt/tasks/karma.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
       singleRun: true
     },
     travis_ci: {
-      browsers: ['firefox'],
+      browsers: ['Firefox'],
       reporters: ['mocha'],
       singleRun: true
     }
